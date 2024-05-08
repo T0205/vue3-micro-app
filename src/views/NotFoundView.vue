204d7b4ff5e632a72245641fc1b035d664e094b1
唐0205 <1507660633@qq.com>
2024-05-08 19:28:12 +0800

    feat: 子应用 url 改造

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>404</h2>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { onMounted } from 'vue';
+
+onMounted(() => { })
+
+</script>
+
+<style lang='scss' scoped></style>
